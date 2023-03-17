c3e79ff72e9db8ab2ed963e2fb08e811f353c144
Aditya Gangil <111217346+adityagangil@users.noreply.github.com>
2023-03-17 10:50:51 +0530

    Add files via upload

---
+++ Pallindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        
+        int reversedNum = 0;
+        int originalNum = num;
+        
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+        
+        if (originalNum == reversedNum) {
+            System.out.println(originalNum + " is a palindrome number.");
+        } else {
+            System.out.println(originalNum + " is not a palindrome number.");
+        }
+    }
+}
